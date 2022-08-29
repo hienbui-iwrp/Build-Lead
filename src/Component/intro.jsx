62c84cf6e5147c06263d7ad2fee25857a25c588f
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2022-08-29 23:23:33 +0700

    update

--- src/Component/intro.jsx
+++ src/Component/intro.jsx
@@ -5,28 +5,46 @@ import { Grid, Image, Stack, Text } from '@mantine/core';
 
 
 export default function Intro() {
+
+    useEffect(() => {
+        const observerCard1 = new IntersectionObserver((entries) => {
+            for (const entry of entries) { // Loop over all elements that either enter or exit the view.
+                if (entry.isIntersecting) { // This is true when the element is in view.
+                    entry.target.classList.add('intro__text--animation'); // Add a class.
+                }
+            }
+        });
+        for (const item of document.querySelectorAll('.intro__text--italic')) {
+            observerCard1.observe(item);
+        }
+    }, [])
+
+
+
     return <Stack spacing="0" className="intro">
         <Stack spacing="xs" className="intro__main">
             <div className="intro__text--background">
                 <Text className="intro__text" style={{ paddingBottom: "1.5rem" }}>TƯỞNG TƯỢNG MỘT BUỔI SÁNG THỨC DẬY</Text>
-                <Text className="intro__text intro__text--italic"> BẠN KHÔNG CÒN MỆT MỎI VÌ ÁP LỰC CÔNG VIỆC, KHÓI BỤI THÀNH PHỐ</Text>
-                <Text className="intro__text intro__text--italic" >KHUNG CẢNH NGAY TRƯỚC MẶT BẠN LÚC NÀY LÀ NÚI NON ĐẤT TRỜI HÙNG VĨ</Text>
+                <Text className="intro__text intro__text--italic">
+                    BẠN KHÔNG CÒN MỆT MỎI VÌ ÁP LỰC CÔNG VIỆC, KHÓI BỤI THÀNH PHỐ
+                    <br />
+                    KHUNG CẢNH NGAY TRƯỚC MẶT BẠN LÚC NÀY LÀ NÚI NON ĐẤT TRỜI HÙNG VĨ
+                </Text>
             </div>
         </Stack>
         <Stack className="intro-more" justify="space-between" spacing="5rem">
             <Text className="intro-more__text" >Bạn có hào hứng không?</Text>
-            <ul className="intro-more__list">
-                <li>
+            <Grid className="intro-more__list">
+                <Grid.Col xs="12" sm="4" className="intro-more__child intro-more__child--frist">
                     HÃY CHO BẢN THÂN MÌNH CƠ HỘI <b>THOÁT KHỎI VÒNG VÂY CỦA CUỘC SỐNG BỘN BỀ</b>
-                </li>
-                <li>
+                </Grid.Col>
+                <Grid.Col xs="12" sm="4" className="intro-more__child">
                     TÌM LẠI NIỀM VUI TRONG <b>VIỆC RÈN LUYỆN THỂ CHẤT</b>
-                </li>
-                <li>
+                </Grid.Col>
+                <Grid.Col xs="12" sm="4" className="intro-more__child">
                     TÌM VỀ VỚI <b>THIÊN NHIÊN</b>
-
-                </li>
-            </ul>
+                </Grid.Col>
+            </Grid>
         </Stack>
         <Stack className="intro-story">
             <div className="intro-story__bg">
